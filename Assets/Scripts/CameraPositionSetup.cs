591a7ca900985631ced4bf29233ca0ec21c5d6c3
Андрей <39644008+AnderX003@users.noreply.github.com>
2022-01-18 20:26:54 +0200

    Added assertions

--- Assets/Scripts/CameraPositionSetup.cs
+++ Assets/Scripts/CameraPositionSetup.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CameraPositionSetup : MonoBehaviour
 {
@@ -7,7 +8,9 @@ public class CameraPositionSetup : MonoBehaviour
     private void Awake()
     {
         float ratio = Screen.width / (float)Screen.height;
-
+        Assert.IsFalse(ratio >= 3/4f);
+        Assert.IsFalse(ratio <= 1/2f);
+        
         cameraTransform.position = Vector3.Lerp(
             new Vector3(0, 40f, -17f),
             new Vector3(0, 29f, -20f),
